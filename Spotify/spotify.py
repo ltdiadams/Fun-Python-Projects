ff44b723d8357f1443a67cfeb63c24470ec77e10
Logan DiAdams <ltdiadams@mta.ca>
2020-05-02 15:07:38 -0300

    New and improved! Randomized song with link and album art

--- Spotify/spotify.py
+++ Spotify/spotify.py
@@ -1,6 +1,6 @@
-# Script making use of Spotipy Lib for Spotify to find the top song of the week
-# in the 'Hot Hits Canada' playlist and text me with the Name, Artist and Release Date
-# for a recommendation! Maybe randomize and schedule it to text daily with a new song?
+# Script making use of Spotipy Lib for Spotify to get a random song from the 'Today's Top Hits' playlist
+# and text me with the Name, Artist, Release Date, Song Link and Album Art
+# for a recommendation! Maybe schedule it to text daily with a new song?
 # Much to think about...
 
 from twilio.rest import Client
@@ -9,16 +9,19 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 import time
 import schedule
+from random import randrange
 
+# credentials for spotify dev account
 client_id = 'YOUR_CLIENT_ID'
 client_secret = 'YOUR_SECRET'
 
+# credentials for your twilio number
 account_sid = 'YOUR_SID'
 auth_token = 'YOUR_TOKEN'
 
 client = Client(account_sid, auth_token)
-sender = 'TWILIO_NUM'
-reciever = 'MY_NUM'
+sender = 'YOUR_TWILIO_NUM'
+reciever = 'YOUR_RECIPIENT'
 
 client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
@@ -31,26 +34,28 @@ def getTrackIDs(user, playlist_id):
         ids.append(track['id'])
     return ids
 
-ids = getTrackIDs('Spotify', '37i9dQZF1DWXT8uSSn6PRy') # Hot Hits Canada Playlist
+# get access to playlist
+ids = getTrackIDs('Spotify', '37i9dQZF1DXcBWIGoYBM5M') # Today's Top Hits Playlist
 
+# get track information
 def getTrack(id):
   meta = sp.track(id)
+  features = sp.audio_features(id)
 
   name = meta['name']
-  # album = meta['album']['name']
   artist = meta['album']['artists'][0]['name']
   release_date = meta['album']['release_date']
 
-  track = [name, artist, release_date]
+  uri = features[0]['uri'] # uri is a special link to a track but needs to be hyperlink
+  url = "https://open.spotify.com/track/" + uri[14:] # turns uri for a track into https
+  art = meta['album']['images'][0]['url'] # gets album art
+
+  track = [name, artist, release_date, url, art]
   return track
 
-# get top track, functionality to do more in the future though...
-track = getTrack(ids[0])
+# get random track in playlist of 50 songs
+track = getTrack(ids[randrange(50)])
 
-text = "Check out Canada's top hit: " + track[0] + " by " + track[1] + "!! Released: " + track[2]
-client.messages.create(to=reciever, from_=sender, body=text)
-# print(text)
-
-# create dataset
-# df = pd.DataFrame(tracks, columns = ['name', 'album', 'artist', 'release_date', 'length', 'popularity'])
-# df.to_csv("spotify.csv", sep = ',')
+# compose text message complete with song link and album cover
+text = "Check out today's top hits: " + track[0] + " by " + track[1] + "!! Released: " + track[2] + " " + track[3]
+client.messages.create(to=reciever, from_=sender, body=text, media_url=track[4])
